247b1d65b620c6de604ad07f85fd02376149df76
Arnau Díaz <diazarnau0@gmail.com>
2020-01-12 16:59:05 +0100

    part01

--- src/part01.rs
+++ src/part01.rs
@@ -33,12 +33,15 @@ fn compute_stuff(x: i32) -> i32 {
 // Let us now refactor `vec_min`.
 fn vec_min(v: Vec<i32>) -> NumberOrNothing {
     fn min_i32(a: i32, b: i32) -> i32 {
-        unimplemented!()
+        if a < b { a } else { b }
     }
 
     let mut min = Nothing;
     for e in v {
-        unimplemented!()
+        min = Number(match min {
+            Nothing => e,
+            Number(n) => min_i32(e, n),
+        })
     }
     min
 }
@@ -62,13 +65,29 @@ fn read_vec() -> Vec<i32> {
 }
 pub fn main() {
     let vec = read_vec();
-    let min = vec_min(vec);
-    unimplemented!()
+    //let min = vec_min(vec);
+    //min.print();
+    let sum = vec_sum(vec);
+    println!("{}", sum);
+    //vec_print(vec);
 }
 // You will have to replace `part00` by `part01` in the `main` function in
 // `main.rs` to run this code.
 
 // **Exercise 01.1**: Write a function `vec_sum` that computes the sum of all values of a `Vec<i32>`.
 
+pub fn vec_sum(vec: Vec<i32>) -> i32 {
+    let mut sum = 0;
+    for e in vec {
+        sum += e
+    }
+    sum
+}
+
 // **Exercise 01.2**: Write a function `vec_print` that takes a vector and prints all its elements.
 
+pub fn vec_print(vec: Vec<i32>) {
+    for e in vec {
+        println!("{}", e);
+    }
+}
