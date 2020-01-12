52a59fdfc7e20f1e0f446aff22485b3cb81f5f67
Arnau Díaz <diazarnau0@gmail.com>
2020-01-12 16:38:30 +0100

    part 00

--- src/part00.rs
+++ src/part00.rs
@@ -23,12 +23,12 @@ fn vec_min(vec: Vec<i32>) -> NumberOrNothing {
             // In this case (*arm*) of the `match`, `min` is currently nothing, so let's just make
             // it the number `el`.
             NumberOrNothing::Nothing => {
-                unimplemented!()
+                min = Number(el);
             },
             // In this arm, `min` is currently the number `n`, so let's compute the new minimum and
             // store it.
             NumberOrNothing::Number(n) => {
-                unimplemented!()
+                min = Number(min_i32(n, el));
             }
         }
     }
@@ -39,9 +39,9 @@ fn vec_min(vec: Vec<i32>) -> NumberOrNothing {
 // Now that we reduced the problem to computing the minimum of two integers, let's do that.
 fn min_i32(a: i32, b: i32) -> i32 {
     if a < b {
-        unimplemented!()
+        a
     } else {
-        unimplemented!()
+        b
     }
 }
 
@@ -57,13 +57,22 @@ use self::NumberOrNothing::{Number,Nothing};
 // a list of numbers, but for now, let's just hard-code something.
 
 fn read_vec() -> Vec<i32> {
-    unimplemented!()
+    vec![3, 5]
 }
 
 // Of course, we would also like to actually see the result of the computation, so we need to print the result.
 
 fn print_number_or_nothing(n: NumberOrNothing) {
-    unimplemented!()
+    match n {
+        NumberOrNothing::Nothing => {
+            println!("Number: Nothing")
+        },
+        // In this arm, `min` is currently the number `n`, so let's compute the new minimum and
+        // store it.
+        NumberOrNothing::Number(n) => {
+            println!("Number: {}", n)
+        }
+    }
 }
 
 // Putting it all together:
